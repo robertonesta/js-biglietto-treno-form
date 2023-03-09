0d61617d0971ac5de4ac5089d4e5e46c88532ca9
RobertoNesta <robertonesta1993@gmail.com>
2023-03-09 16:13:56 +0100

    creating kms and ages consts

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,9 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.*/
 
+const submitbutton = document.getElementById("submit");
+
+submitbutton.addEventListener('click', function(){
+    const kilometers = Number(document.querySelector('input[name="km"]').value);
+const userAge = document.getElementById("età").value;
+}) 
